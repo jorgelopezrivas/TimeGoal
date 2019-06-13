699094ea813223a13fdefab768ede342e1d8aac9
Jorge L. Rivas <jorge.lopez@karalundi.com.mx>
2019-06-13 08:33:52 -0500

    placeholder screens

---
+++ src/screens/GoalList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class GoalList extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Goal List </Text>
+            </View>
+        );
+    }
+}
