fb7cd6f408a89416efc76a9125a945aa3fa4e33e
Jorge L. Rivas <jorge.lopez@karalundi.com.mx>
2019-06-13 08:34:19 -0500

    react navigation app setup

--- App.js
+++ App.js
@@ -1,49 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+// @flow
+import React, { Component } from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import GoalList from './src/screens/GoalList';
+import AddGoal from './src/screens/AddGoal';
+import EditGoal from './src/screens/EditGoal';
+import GoalMain from './src/screens/GoalMain';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+const navOptions = {
+    gesturesEnabled: true
+};
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-        'Double tap R on your keyboard to reload,\n' +
-        'Shake or press menu button for dev menu',
-});
+const AppStack = createStackNavigator({
+    GoalList,
+    AddGoal,
+    EditGoal,
+    GoalMain
+},
+{ defaultNavigationOptions: navOptions});
+
+const AppContainer = createAppContainer(AppStack);
 
 type Props = {};
+
 export default class App extends Component<Props> {
     render(){
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>Welcome to React Native!</Text>
-                <Text style={styles.instructions}>To get started, edit App.js</Text>
-                <Text style={styles.instructions}>{instructions}</Text>
-            </View>
+            <AppContainer />
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
